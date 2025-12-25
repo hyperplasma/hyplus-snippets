7e815dd17db550f279e65fbe3087f6fcbf640a68
Akira37 <863781751@qq.com>
2025-12-25 17:30:57 +0800

    优化页脚版权信息，移除备案链接并调整格式

--- src/minor/security/footer_beian_text.php
+++ src/minor/security/footer_beian_text.php
@@ -13,6 +13,9 @@ add_filter('the_generator', 'wpbeginner_remove_version');
 add_filter( 'generate_copyright','tu_custom_copyright' );
 function tu_custom_copyright() {
     ?>
-© 2024-<?php echo date("Y"); ?> <a href="https://www.hyperplasma.top/about/">Hyperplasma</a> • <a href="https://www.hyperplasma.top/privacy-policy/">隐私政策</a> • <a href="https://www.hyperplasma.top/hyplus/terms/">服务条款</a><br><a href="http://beian.miit.gov.cn/" target="_blank">XXX</a> <img src="https://www.hyperplasma.top/wp-content/uploads/2024/08/备案图标.png" width=15px height=15px> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=XXX" rel="noreferrer" target="_blank">XXX</a>
+© 2024-<?php echo date("Y"); ?> <a href="https://www.hyperplasma.top/about/">Hyperplasma</a> • <a href="https://www.hyperplasma.top/privacy-policy/">隐私政策</a> • <a href="https://www.hyperplasma.top/hyplus/terms/">服务条款</a>
+<?php
+ // <br><a href="http://beian.miit.gov.cn/" target="_blank">XXX</a> <img src="https://www.hyperplasma.top/wp-content/uploads/2024/08/备案图标.png" width=15px height=15px> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=XXX" rel="noreferrer" target="_blank">XXX</a> 
+?>
     <?php
 }
