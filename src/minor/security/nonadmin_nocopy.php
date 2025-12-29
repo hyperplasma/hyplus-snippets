6c9b73fcd44e7e61b924fb74085acee13d48d5da
Akira37 <863781751@qq.com>
2025-12-29 16:46:05 +0800

    Change check of admin to only log-in

--- src/minor/security/nonadmin_nocopy.php
+++ src/minor/security/nonadmin_nocopy.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Non-admin No Copy php
+ * Non-admin No Copy (currently only check if logged in) php
  * Current status: fluid
  */
 add_action('wp_head', function () {
-    // 检查用户是否为管理员
-    if (!current_user_can('administrator')) {
+    // 检查用户是否已经登陆
+    // if (!current_user_can('administrator')) {
+    if (!is_user_logged_in()) {
         echo '<style>article{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}</style>';
     }
 });
