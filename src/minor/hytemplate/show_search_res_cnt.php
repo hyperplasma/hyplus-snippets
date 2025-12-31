56944943730244e90a3b9d486637093b9ce85e23
Akira37 <863781751@qq.com>
2025-12-31 22:03:51 +0800

    Rename search res cnt php; remove obsoleted old cnt nav php

--- src/minor/hytemplate/show_search_res_cnt.php
+++ src/minor/hytemplate/show_search_res_cnt.php
@@ -1,37 +1,6 @@
 <?php
 /**
- * Show Post Counts in Nav bar (unused currently) and Search Results Count
- * Code type: PHP
- */
-// // 获取分类或标签下的文章数量
-// function get_category_post_count($cat_id) {
-//     $category = get_category($cat_id);
-//     return $category->category_count;
-// }
-
-// // 在导航菜单项后追加文章数量
-// function add_post_count_to_menu_item($title,$item, $args) {
-//     // 检查是否是主菜单
-//     if ($args->theme_location == 'primary') {
-//         // 检查菜单项是否有分类ID设置
-//         if (get_post_meta($item->ID, '_menu_item_object_id', true)) {
-//             $category_id = get_post_meta($item->ID, '_menu_item_object_id', true);
-//             $post_count = get_category_post_count($category_id);
-//             // 当且仅当有文章时才追加显示文章数量
-//             if ($post_count > 0) {
-//                 $title .= ' (' .$post_count . ')';
-//             }
-//         }
-//     }
-//     return $title;
-// }
-
-// // 添加过滤器钩子
-// add_filter('nav_menu_item_title', 'add_post_count_to_menu_item', 10, 3);
-
-
-/**
- * Show Search Result Count and Mobile Search Form - 只在前台搜索结果页生效
+ * Show Search Result Count and Mobile Search Form 
  * Code type: PHP
  */
 add_action('template_redirect', function () {
