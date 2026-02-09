833ea0566b4947dd698e3af02d437e8a96c96252
Akira37 <863781751@qq.com>
2026-02-09 23:29:42 +0800

    Add att snippet

---
+++ minor/security/disable_attachment_pages.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Disable Attachment Pages
+ * Description: Hide the Attachment/Attachments pages on the frontend from all visitors.
+ * Code type: PHP
+ */
+add_action(
+	'template_redirect',
+	function () {
+		global $post;
+		if ( ! is_attachment() || ! isset( $post->post_parent ) || ! is_numeric( $post->post_parent ) ) {
+			return;
+		}
+
+		// Does the attachment have a parent post?
+		// If the post is trashed, fallback to redirect to homepage.
+		if ( 0 !== $post->post_parent && 'trash' !== get_post_status( $post->post_parent ) ) {
+			// Redirect to the attachment parent.
+			wp_safe_redirect( get_permalink( $post->post_parent ), 301 );
+		} else {
+			// For attachment without a parent redirect to homepage.
+			wp_safe_redirect( get_bloginfo( 'wpurl' ), 302 );
+		}
+		exit;
+	},
+	1
+);
+?>
